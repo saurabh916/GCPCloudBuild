bd8b57be955d66e1f5cee742aaf430cb1ceebe6c
saurabh916 <saurabh916@gmail.com>
2020-01-22 22:25:54 +0100

    added for cloud build

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var handleRequest = function(request, response) {
+  response.writeHead(200);
+  response.end("<h1> Deployed Version is v1.1 </h1>");
+}
+var www = http.createServer(handleRequest);
+www.listen(3000);
