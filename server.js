286f86dad964aa1c89aa43950458da5f603283f6
saurabh916 <saurabh916@gmail.com>
2020-01-25 17:55:37 +0100

    added modified server.js for cloud build

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1> Deployed Version is v1.1 </h1>");
+  response.end("<h1> Deployed Version is v1.1 using cloud build </h1>");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
